7c91f77888a78cb7bec0b1ab5ddab1bf704fba8c
agnivarna trivedi <trivediagnivarna1999@gmail.com>
2018-06-21 15:30:09 +0530

    firstcommit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to:'discussions#index'
+  resources :discussions
+
+  get '/auth/:provider/callback' => 'sessions#new'
+  get '/auth/sign-out' => 'sessions#sign_out', as: :sign_out
+end
